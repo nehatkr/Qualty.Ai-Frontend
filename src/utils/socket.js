1db745ea32bebef173bc8d464dba72fd5928517b
Aswin Krishna <aswinkrishna005@gmail.com>
2025-10-21 08:58:34 +0530

    socket.io production path change

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,5 +2,16 @@ import { io } from "socket.io-client";
 import {BASE_URL} from "./constants"
 
  export const createSocketConnection = ()=>{
-    return io(BASE_URL)
+
+   if(location.hostname==="localhost"){
+
+      return io(BASE_URL)
+   }
+
+   return io("https://qualty.ai",{
+      path:"/socket.io",
+      transports:["websocket","polling"],
+      withCredentials:true
+   })
+
  }
